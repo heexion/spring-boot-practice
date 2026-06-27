af8159cb5fc2950a841143d8d33988eea69235aa
heexion <alsgml030901@naver.com>
2026-06-27 14:32:58 +0900

    FEAT: 팀 엔티티 추가

---
+++ src/main/java/com/example/springbootpractice/entity/Team.java
@@ -0,0 +1,23 @@
+package com.example.springbootpractice.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Team {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "team")
+    private List<Member> members = new ArrayList<>();
+}
